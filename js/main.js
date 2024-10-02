ab4731fbc28d2e34bb4a3be42d6539fbee97b5ff
Kristan Villavieja <kvillavieja1@nait.ca>
2024-10-01 22:47:37 -0600

    Submitting Lab 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 // Enter your code below.
+(function () {
+    const resources = document.querySelector('.javascript-resources');
+
+    document.querySelector('#show-resources').addEventListener('click', () => {
+        resources.classList.remove('d-none');
+    });
+
+    document.getElementById('#show-resources').addEventListener('mouseover', (event)=>{
+        console.log(event.target.classList);
+        event.target.classList.add('fw-bold');
+    });
+
+    document.getElementById('#show-resources').addEventListener('mouseout', (event)=> {
+        event.target.classList.remove('fw-bold');
+    });
+
+    document.querySelector('.list-group-item').addEventListener('click', (event)=> {
+        event.target.classList.add('fst-italic');
+    })
+
+})();
